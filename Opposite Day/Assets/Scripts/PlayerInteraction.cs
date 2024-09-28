02f31f7024d2301e6d6902894bbee5d3c3dbd707
sndman13 <124104254+sndman13@users.noreply.github.com>
2024-09-28 13:39:35 -0700

    Update script

--- Opposite Day/Assets/Scripts/PlayerInteraction.cs
+++ Opposite Day/Assets/Scripts/PlayerInteraction.cs
@@ -9,16 +9,18 @@ public class PlayerInteraction : MonoBehaviour
     public GameObject exhibitText2;
     public GameObject exhibitText3;
     public GameObject exhibitText4;
+    public GameObject exhibitText5;
     int cycle;
+    public int maxCycle;
 
         public void OnTriggerEnter(Collider other)
         {
-            if (other.CompareTag("Exhibit"))
+            if (other.CompareTag("Player"))
             {
                 triggerActive = true;
+                Debug.Log("Collided");
             }
         }
-
         public void OnTriggerExit(Collider other)
         {
             if (other.CompareTag("Player"))
@@ -28,34 +30,48 @@ public class PlayerInteraction : MonoBehaviour
                 exhibitText2.SetActive(false);
                 exhibitText3.SetActive(false);
                 exhibitText4.SetActive(false);
+                exhibitText5.SetActive(false);
                 cycle = 1;
             }
         }
-
         private void Update()
         {
             if (triggerActive && Input.GetKeyDown(KeyCode.Space))
             {
-                if (cycle == 1)
+                exhibitText1.SetActive(false);
+                exhibitText2.SetActive(false);
+                exhibitText3.SetActive(false);
+                exhibitText4.SetActive(false);
+                exhibitText5.SetActive(false);
+                if (cycle == 5)
+                {
+                    exhibitText5.SetActive(true);
+                    cycle += 1;
+                }
+                else if (cycle == 4)
+                {
+                    exhibitText4.SetActive(true);
+                    cycle += 1;
+                }
+                else if (cycle == 3)
+                {
+                    exhibitText3.SetActive(true);
+                    cycle += 1;
+                }
+                else if (cycle == 2)
+                {
+                    exhibitText2.SetActive(true);
+                    cycle += 1;
+                }
+                else if (cycle == 1)
                 {
                     exhibitText1.SetActive(true);
                     cycle += 1;
                 }
-                if (cycle == 2)
-                {
-                    exhibitText1.SetActive(true);
-                    cycle += 2;
-                }
-                if (cycle == 3)
-                {
-                    exhibitText1.SetActive(true);
-                    cycle += 3;
-                }
-                if (cycle == 4)
-                {
-                    exhibitText1.SetActive(true);
-                    cycle = 1;
-                }
+            }
+            if (cycle >= maxCycle)
+            {
+                cycle = 1;
             }
         }
 }
