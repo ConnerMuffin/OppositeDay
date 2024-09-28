840686a6b79843b99f36048b172816e3d7c04093
sndman13 <124104254+sndman13@users.noreply.github.com>
2024-09-28 12:55:07 -0700

    Update Player Interaction

--- Opposite Day/Assets/Scripts/PlayerInteraction.cs
+++ Opposite Day/Assets/Scripts/PlayerInteraction.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerInteraction : MonoBehaviour
 {
     bool triggerActive = false;
-    public GameObject exhibitText;
+    public GameObject exhibitText1;
+    public GameObject exhibitText2;
+    public GameObject exhibitText3;
+    public GameObject exhibitText4;
+    int cycle;
 
         public void OnTriggerEnter(Collider other)
         {
@@ -20,7 +24,11 @@ public class PlayerInteraction : MonoBehaviour
             if (other.CompareTag("Player"))
             {
                 triggerActive = false;
-                exhibitText.SetActive(false);
+                exhibitText1.SetActive(false);
+                exhibitText2.SetActive(false);
+                exhibitText3.SetActive(false);
+                exhibitText4.SetActive(false);
+                cycle = 1;
             }
         }
 
@@ -28,7 +36,26 @@ public class PlayerInteraction : MonoBehaviour
         {
             if (triggerActive && Input.GetKeyDown(KeyCode.Space))
             {
-                exhibitText.SetActive(true);
+                if (cycle == 1)
+                {
+                    exhibitText1.SetActive(true);
+                    cycle += 1;
+                }
+                if (cycle == 2)
+                {
+                    exhibitText1.SetActive(true);
+                    cycle += 2;
+                }
+                if (cycle == 3)
+                {
+                    exhibitText1.SetActive(true);
+                    cycle += 3;
+                }
+                if (cycle == 4)
+                {
+                    exhibitText1.SetActive(true);
+                    cycle = 1;
+                }
             }
         }
 }
